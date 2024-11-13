8c8fc59fe0d2eef882534cb8fdc5641e6934c666
Salsa <Salsa@SALSAPC>
2024-11-12 22:55:19 -0300

    Adicionar arquivos de projeto.

---
+++ Controllers/AutorController.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApiLivros.Dto.Autor;
+using WebApiLivros.Models;
+using WebApiLivros.Services.Autor;
+
+namespace WebApiLivros.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AutorController : ControllerBase
+    {
+
+        private readonly IAutorInterface _autorInterface;
+        public AutorController(IAutorInterface autorInterface)
+        {
+            _autorInterface = autorInterface;
+        }
+
+
+        [HttpGet("ListarAutores")]
+        public async Task<ActionResult<ResponseModel<List<AutorModel>>>> ListaAutores()
+        {
+            var autores = await _autorInterface.ListarAutores();
+            return Ok(autores);
+        }
+
+        [HttpGet("BuscarAutorPorId/{idAutor}")]
+        public async Task<ActionResult<ResponseModel<AutorModel>>> BuscarAutorPorId(int idAutor )
+        {
+            var autor = await _autorInterface.BuscarAutorPorId(idAutor);
+            return Ok(autor);
+
+        }
+
+        [HttpGet("BuscarAutorPorIdLivro/{idLivro}")]
+        public async Task<ActionResult<ResponseModel<AutorModel>>> BuscarAutorPorIdLivro(int idLivro)
+        {
+            var autor = await _autorInterface.BuscarAutorPorIdLivro(idLivro);
+            return Ok(autor);
+
+        }
+
+        [HttpPost("CriarAutor")]
+        public async Task<ActionResult<ResponseModel<List<AutorModel>>>> CriarAutor(AutorCriacaoDto autorCriacaoDto)
+        {
+            var autores = await _autorInterface.CriarAutor(autorCriacaoDto);
+            return Ok(autores);
+        }
+
+        [HttpPut("EditarAutor")]
+        public async Task<ActionResult<ResponseModel<List<AutorModel>>>> EditarAutor(AutorEdicaoDto autorEdicaoDto)
+        {
+            var autores = await _autorInterface.EditarAutor(autorEdicaoDto);
+            return Ok(autores);
+        }
+
+        [HttpDelete("ExcluirAutor")]
+        public async Task<ActionResult<ResponseModel<List<AutorModel>>>> ExcluirAutor(int IdAutor)
+        {
+            var autores = await _autorInterface.ExcluirAutor(IdAutor);
+            return Ok(autores);
+        }
+    }
+}
---
+++ Services/Autor/IAutorInterface.cs
@@ -0,0 +1,16 @@
+﻿using WebApiLivros.Dto.Autor;
+using WebApiLivros.Models;
+
+namespace WebApiLivros.Services.Autor
+{
+    public interface IAutorInterface
+    {
+        Task<ResponseModel<List<AutorModel>>> ListarAutores();
+        Task<ResponseModel<AutorModel>> BuscarAutorPorId(int idAutor);
+        Task<ResponseModel<AutorModel>> BuscarAutorPorIdLivro(int idLivro);
+        Task<ResponseModel<List<AutorModel>>> CriarAutor(AutorCriacaoDto AutorCriacaoDto);
+        Task<ResponseModel<List<AutorModel>>> EditarAutor(AutorEdicaoDto autorEdicaoDto);
+        Task<ResponseModel<List<AutorModel>>> ExcluirAutor(int idAutor);
+
+    }
+}
